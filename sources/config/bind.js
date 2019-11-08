aedace2dd3b1ff180b19d1087e0890324175036c
Movchan Vlad <50574084+sln4488@users.noreply.github.com>
2019-11-08 04:15:41 +0200

    Update bind.js

--- sources/config/bind.js
+++ sources/config/bind.js
@@ -1,22 +1,25 @@
-document.querySelector('#add').onclick = function() {
+// THIS PIECE OF CODE I BINT MY BUTTONS TO SPECIFIC ACTIONS
+
+document.querySelector('#add').onclick = function() {  // BUTTON "CREATE"
 	document.querySelector(".container-wrapper").style.display = 'flex';
 }
 
-document.querySelector('#cancel').onclick = function() {
+document.querySelector('#cancel').onclick = function() {  // BUTTON "CANCEL" IN RECORD FORM BY NOTATES [1]
 	document.querySelector(".container-wrapper").style.display = 'none';
 	document.querySelector('#title').value = '';
 	document.querySelector('#description').value = '';
 	document.querySelector('#selectvalue').value = 'high';
 }
 
-document.querySelector('#cancel_proto').onclick = function() {
+document.querySelector('#cancel_proto').onclick = function() {  // [1] BY "EDIT" NOTATES
 	document.querySelector("#wrapper_proto").style.display = 'none';
 }
 
-document.querySelector('#message').onclick = function() {
+document.querySelector('#message').onclick = function() {  // FOR MY SEARCH
 	alert("Your search have liveSearch :)");
 }
 
+// FUNCTION WHEN I CLOSE MY MODAL PAGE ON "ESC"
 // document.addEventListener('keypress', function (e) {
 //    if(e.keyCode === 27) document.getElementById('wrapper').style.display = 'none';
 // }); 
