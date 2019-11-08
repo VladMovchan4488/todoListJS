4c4134d3c2af8f36f1dc03dcfbf100fc4aef9667
Movchan Vlad <50574084+sln4488@users.noreply.github.com>
2019-11-08 03:57:46 +0200

    Update delete_item.js

--- sources/dropdown_btn/delete_item.js
+++ sources/dropdown_btn/delete_item.js
@@ -1,12 +1,14 @@
 // This function remove item in HTML/CLASS/LOCAL STORAGE
 function delete_item() {
-	const delete_item = this.parentNode.parentNode.parentNode.parentNode.getAttribute("id"); // bad solutions
-	this.parentNode.parentNode.parentNode.parentNode.remove();
-	todo.items.forEach(function(item, index, object) {
-      if (item.id == delete_item) {
-         object.splice(index, 1);
-      }
-   });
+	const delete_item = this.parentNode.parentNode.parentNode.parentNode.getAttribute("id"); // bad solutions, I understand this
+	
+	this.parentNode.parentNode.parentNode.parentNode.remove(); // remove in html
+	
+	todo.items.forEach(function(item, index, object) { // remove in class
+     		if (item.id == delete_item) {
+        		object.splice(index, 1);
+		}
+	});
    // var serialObj = JSON.stringify(todo.items);   // toLocal (the same)
-	// localStorage.setItem("myKey", serialObj);    // 
+   // localStorage.setItem("myKey", serialObj);    // remove in localStorage
 }
