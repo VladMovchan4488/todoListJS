94408a4659f744fe346ad48aff3066795c50b98b
Movchan Vlad <50574084+sln4488@users.noreply.github.com>
2019-11-08 04:18:41 +0200

    Update validation.js

--- sources/config/validation.js
+++ sources/config/validation.js
@@ -1,6 +1,8 @@
+// THIS FUNCTION FOR VALIDATE MY TITLE 
+
 function validateTitle(title) {
 	
-	const isExist = todo.items.find(e => {
+	const isExist = todo.items.find(e => {   // VALIDATE SAME TITLE
 		return e.id == title.replace(/\s/g, '');
 	});
 
@@ -12,14 +14,16 @@ function validateTitle(title) {
 		return false;
 	}
 
-	if(title == "" || title >= 0 || title <= 0) {
+	if(title == "" || title >= 0 || title <= 0) {  // BAD SOLUTIONS FOR NUMBERS, I UNDERSTANT THIS
+						       // VALIDATE NULL TITLE AND NUMBERS
 		alert("Hmm.. I don't be empty, first letters can be only (a-z/а-я) :( And don't wtite the same title please :)");
 		document.querySelector('#title').value = 'LoL';
 		document.querySelector('#description').value = '';
 		document.querySelector('#selectvalue').value = 'high';
 		return false;
 	}
-
+	
+	// VALIDATE FOR OTHER MANY SYMBOLS
 	// if (! /^$/.test(title)) {
 	// 	alert("Hmm.. I don't be empty, first letters can be only (a-z/а-я) :( And don't wtite the same title please :)");
 	// 	document.querySelector('#title').value = 'LoL';
@@ -27,12 +31,9 @@ function validateTitle(title) {
 	// 	document.querySelector('#selectvalue').value = 'high';
 	// 	return false;
 	// }	
-
-	// if ( title.indexOf('') > -1 )  { alert( "found it" );  } 
-
+	
 	else 
 	{
 		return true;
 	}
-
 }
