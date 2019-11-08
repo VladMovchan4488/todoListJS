9304854fe1ae3f79ed538dbc8cb194e009ce8b00
Movchan Vlad <50574084+sln4488@users.noreply.github.com>
2019-11-08 04:01:01 +0200

    Update edit_item.js

--- sources/dropdown_btn/edit_item.js
+++ sources/dropdown_btn/edit_item.js
@@ -1,15 +1,18 @@
+// THIS PIECE OF CODE ABOUT BUTTON "EDIT" MY NOTATION
+
 function edit_item() {
 
   document.querySelector("#wrapper_proto").style.display = 'flex';
-  const edit_item = this.parentNode.parentNode.parentNode.parentNode;
+  const edit_item = this.parentNode.parentNode.parentNode.parentNode; //BAD SOLUTION, I UNDERSTAND THIS
   let elementId = edit_item.getAttribute('id');
   
+  // GET DATA IN OLD FORMS AND RECORD IN NEW (HTML)
   document.getElementById('title_proto').value = edit_item.querySelector('.name_item').innerText; 
   document.getElementById('description_proto').value = edit_item.querySelector('.description_item').innerText; 
   document.getElementById('selectvalue_proto').value = edit_item.querySelector('.priority_item').innerText;
 
+  // GET DATA IN OLD FORMS AND RECORD IN NEW (JS) [2]
   document.getElementById('save_proto').onclick = function () {
-
   	var rwTitle = document.getElementById('title_proto').value;
   	edit_item.querySelector('.name_item').innerText = rwTitle; 
 
@@ -22,7 +25,8 @@ function edit_item() {
   	document.querySelector("#wrapper_proto").style.display = 'none';
 
   	todo.items.forEach(e => {
-
+      
+      // [2]
   		if(elementId == e.id) {
   			e.title = document.getElementById('title_proto').value;
   			e.description = document.getElementById('description_proto').value;
