edb8f242ad03e2a20f19de04499e29c7e72fce67
Movchan Vlad <50574084+sln4488@users.noreply.github.com>
2019-11-08 04:07:33 +0200

    Update check_item.js

--- sources/dropdown_btn/check_item.js
+++ sources/dropdown_btn/check_item.js
@@ -1,45 +1,52 @@
+// THIS PIECE OF CODE ABOUT BUTTON "DONE/OPEN" + CHECKBOX
+
 function check_item(item) {  
 
       const btn = item.querySelector('.checkButton');
 		const checkBtn = item.querySelector('.checkBox');
-
+		
+		// CHANGE TEXT FOR BUTTON IN HTML [1]
 		if (btn.innerHTML == "open") {			 
 			btn.innerHTML = "done";    
-
+			
+			// CHANGE COLOR: BUTTON + THIS NOTATION [2]
 			item.style.background = '#fff';
 			btn.style.background = '#c0c0c0';
 			checkBtn.style.display = 'none';
-
-			todo.items.forEach(e => {
+			
+			// CHANGE TEXT FOR BUTTON IN JS [3]
+			todo.items.forEach(e => { 
 				if( e.id == item.getAttribute('id') ){
 					e.check = "done";
 				} 	
        	}); 	
-       	allFilter();
+       	allFilter(); // DINAMIC CALL TO FILTER, IF CHECKBOX == TRUE [4]
       }
 
   		else {
-			btn.innerHTML = "open"; 
-
+			btn.innerHTML = "open"; // [1]
+			
+			// [2]
 			checkBtn.style.display = 'flex';
 			item.style.background = '#c0c0c0';
 			btn.style.background = '#fff';
-
+			
+			// [3]
 			todo.items.forEach(e => {
 				if( e.id == item.getAttribute('id') ){
 					e.check = "open";
 				}
        	});
-       	allFilter();
+       	allFilter(); // [4]
 		}
 
 }
 
-
+// FUNCTION BY CHECKBOX CHANGE
 function check_item_box(checkbox) {
 	const item = checkbox.parentNode;
 	const checkBtn = item.querySelector('.checkButton');
-
+	
 	item.style.background = '#fff';
 	checkBtn.style.background = '#c0c0c0';
 	checkbox.style.display = 'none';
@@ -49,9 +56,9 @@ function check_item_box(checkbox) {
 	todo.items.forEach(e => {
 		if( e.id == item.getAttribute('id') ){
 			e.check = "done";
-			allFilter();
+			allFilter(); // [4]
 		} 	
    }); 	
 
-   checkbox.checked = 'checked';
+   checkbox.checked = 'checked'; // CHANGE STATUS CHECKBOX IN THE END OF FUNCTION
 }
